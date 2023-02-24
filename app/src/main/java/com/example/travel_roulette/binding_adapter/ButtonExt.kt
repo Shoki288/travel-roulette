7b332edfbe023fa3dfa65d46d5625c47d4932eac
syooki <gsyoki0719@gmail.com>
2023-02-24 23:06:11 +0900

    レイアウト調整

--- app/src/main/java/com/example/travel_roulette/binding_adapter/ButtonExt.kt
+++ app/src/main/java/com/example/travel_roulette/binding_adapter/ButtonExt.kt
@@ -1,15 +1,20 @@
 package com.example.travel_roulette.binding_adapter
 
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
-import com.example.travel_roulette.vo.SelectablePrefecture
+import com.example.travel_roulette.R
 
-@BindingAdapter("buttonEnabled")
-fun Button.buttonEnabled(enabled: Boolean) {
-    this.isClickable = enabled
-    this.isFocusable = enabled
+@BindingAdapter("selectPrefecture")
+fun Button.selectPrefecture(select: Boolean) {
+    val bgColor = if (select) {
+        R.color.md_theme_light_onSurfaceVariant
+    } else {
+        R.color.white
+    }
+    this.setBackgroundColor(ContextCompat.getColor(context, bgColor))
 }
 
 interface SelectPrefectureListener {
-    operator fun invoke(index: Int): Boolean
+    operator fun invoke(index: Int)
 }
